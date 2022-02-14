137b756b9a1fc2d111dd762d22fdebb12506e7c7
Erick <erguzman96@gmail.com>
2022-02-14 02:27:12 -0600

    feat(*): Header UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+    return (
+        <header className="bg-pink w-screen">
+            <div className="p-3 lg:px-4 2xl:max-w-screen-xl my-0 mx-auto flex items-center justify-between">
+                <Link to="/">
+                    <h1 className="text-xl lg:text-3xl font-semibold text-white">
+                        Ecommerce
+                    </h1>
+                </Link>
+                <div className="flex items-center">
+                    <Link to="/products">
+                        <span className="text-md lg:text-xl font-light text-white mr-4 xl:mr-8">
+                            Productos
+                        </span>
+                    </Link>
+                    <Link to="/cart">
+                        <FontAwesomeIcon
+                            icon={faBagShopping}
+                            className="text-white text-xl lg:text-2xl"
+                        />
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const Layout = () => {
     return (
         <>
-            <div>Layout</div>
+            <Header />
             <Outlet />
         </>
     );
