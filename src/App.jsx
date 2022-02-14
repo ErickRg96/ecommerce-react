10b14b44348d5042232765cfcb55a6e5e8e94a25
Erick <erguzman96@gmail.com>
2022-02-14 01:09:41 -0600

    feat(*): Configure router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-    return <div className="App">App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="product/:id" element={<Product />} />
+                    <Route path="cart" element={<Cart />} />
+                    <Route element={<NotFound />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
